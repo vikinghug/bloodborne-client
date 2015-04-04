11bf1adbaf4708f92780a3bbe8676840e4205938
Kevin Altman <kevin@itsthatguy.com>
2015-04-04 17:50:38 -0400

    stats

---
+++ data/class.js
@@ -0,0 +1,93 @@
+module.exports = [
+  {
+    name        : 'Milquetoast',
+    vitality    : 11,
+    endurance   : 10,
+    strength    : 12,
+    skill       : 10,
+    bloodtinge  : 9,
+    arcane      : 8,
+    bloodEchoes : 300,
+    level       : 10
+  }, {
+    name        : 'Lone Survivor',
+    vitality    : 14,
+    endurance   : 11,
+    strength    : 11,
+    skill       : 10,
+    bloodtinge  : 7,
+    arcane      : 7,
+    bloodEchoes : 420,
+    level       : 10
+  }, {
+    name        : 'Troubled Childhood',
+    vitality    : 9,
+    endurance   : 14,
+    strength    : 9,
+    skill       : 13,
+    bloodtinge  : 6,
+    arcane      : 9,
+    bloodEchoes : 360,
+    level       : 10
+  }, {
+    name        : 'Violent Past',
+    vitality    : 12,
+    endurance   : 11,
+    strength    : 15,
+    skill       : 9,
+    bloodtinge  : 6,
+    arcane      : 7,
+    bloodEchoes : 180,
+    level       : 10
+  }, {
+    name        : 'Professional',
+    vitality    : 9,
+    endurance   : 12,
+    strength    : 9,
+    skill       : 15,
+    bloodtinge  : 7,
+    arcane      : 8,
+    bloodEchoes : 240,
+    level       : 10
+  }, {
+    name        : 'Military Veteran',
+    vitality    : 10,
+    endurance   : 10,
+    strength    : 14,
+    skill       : 13,
+    bloodtinge  : 7,
+    arcane      : 6,
+    bloodEchoes : 320,
+    level       : 10
+  }, {
+    name        : 'Noble Scion',
+    vitality    : 7,
+    endurance   : 8,
+    strength    : 9,
+    skill       : 13,
+    bloodtinge  : 14,
+    arcane      : 9,
+    bloodEchoes : 540,
+    level       : 10
+  }, {
+    name        : 'Cruel Fate',
+    vitality    : 10,
+    endurance   : 12,
+    strength    : 10,
+    skill       : 9,
+    bloodtinge  : 5,
+    arcane      : 14,
+    bloodEchoes : 500,
+    level       : 10
+  }, {
+    name        : 'Waste of Skin',
+    vitality    : 10,
+    endurance   : 9,
+    strength    : 10,
+    skill       : 9,
+    bloodtinge  : 7,
+    arcane      : 9,
+    bloodEchoes : 10,
+    level       : 4
+  }
+];
