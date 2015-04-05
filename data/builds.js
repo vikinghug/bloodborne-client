13667c8ea942fd50625af0dbc40460797942f48b
Kevin Altman <kevin@itsthatguy.com>
2015-04-05 13:30:52 -0400

    more stub data

---
+++ data/builds.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    name          : 'My awesome dexxer',
+    class         : 'Milquetoast',
+    user          : 13,
+    vitality      : 11,
+    endurance     : 10,
+    strength      : 12,
+    skill         : 10,
+    bloodtinge    : 9,
+    arcane        : 8,
+    bloodEchoes   : 300,
+    level         : 10,
+    vote          : 124,
+    commentsCount : 42,
+    comments      : []
+  }, {
+    name          : 'Strength',
+    user          : 12,
+    class         : 'Milquetoast',
+    vitality      : 11,
+    endurance     : 10,
+    strength      : 12,
+    skill         : 10,
+    bloodtinge    : 9,
+    arcane        : 8,
+    bloodEchoes   : 300,
+    level         : 10,
+    vote          : 424,
+    commentsCount : 22,
+    comments      : []
+  }
+];
---
+++ data/users.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    id       : 1,
+    username : 'vikinghug',
+    email    : 'kevin@itsthatguy.com',
+    password : 'password' 
+  }, {
+    id       : 2,
+    username : 'ryland',
+    email    : 'rylnd@gmail.com',
+    password : 'password' 
+  }
+];
