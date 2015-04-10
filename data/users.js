fca74fef1d5ca568ed3231c0a00d7f6b8a2c2601
Kevin Altman <kevin@itsthatguy.com>
2015-04-09 20:34:15 -0400

    data to json

--- data/users.js
+++ data/users.js
@@ -1,13 +1,13 @@
-module.exports = [
+[
   {
-    id       : 1,
-    username : 'vikinghug',
-    email    : 'kevin@itsthatguy.com',
-    password : 'password' 
+    "id"       : 1,
+    "username" : "vikinghug",
+    "email"    : "kevin@itsthatguy.com",
+    "password" : "password" 
   }, {
-    id       : 2,
-    username : 'ryland',
-    email    : 'rylnd@gmail.com',
-    password : 'password' 
+    "id"       : 2,
+    "username" : "ryland",
+    "email"    : "rylnd@gmail.com",
+    "password" : "password" 
   }
-];
+]
