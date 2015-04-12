514f6e8c059d3ec7ec1ecd20047272f760e93422
Kevin Altman <kevin@itsthatguy.com>
2015-04-12 11:53:46 -0400

    plumbs dungeon data

--- data/dungeons.js
+++ data/dungeons.js
@@ -3,16 +3,121 @@
     "id": 0,
     "name": "My First Dungeon",
     "glyph": "lksa81234",
-    "items": []
+    "levels": [
+      {
+        "name": 1,
+        "locations": [{
+          "name": "location 1 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "A Dwaenor's fist hammer" },
+            { "name": "B Golden fistula" },
+            { "name": "C Uncanny Saw Cleaver" }
+          ]
+        }, {
+          "name": "location 2 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "D Dwaenor's fist hammer" },
+            { "name": "E Golden fistula" },
+            { "name": "F Uncanny Saw Cleaver" }
+          ]
+        }]
+      }, {
+        "name": 2,
+        "locations": [{
+          "name": "location 1 - level 2",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 2,
+          "items": [
+            { "name": "Dwaenor's fist hammer" },
+            { "name": "Golden fistula" },
+            { "name": "Uncanny Saw Cleaver" }
+          ]
+        }]
+      }
+    ]
   }, {
     "id": 1,
     "name": "My Second Dungeon",
     "glyph": "1234lksa8",
-    "items": []
+    "levels": [
+      {
+        "name": 1,
+        "locations": [{
+          "name": "location 1 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "A Dwaenor's fist hammer" },
+            { "name": "B Golden fistula" },
+            { "name": "C Uncanny Saw Cleaver" }
+          ]
+        }, {
+          "name": "location 2 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "D Dwaenor's fist hammer" },
+            { "name": "E Golden fistula" },
+            { "name": "F Uncanny Saw Cleaver" }
+          ]
+        }]
+      }, {
+        "name": 2,
+        "locations": [{
+          "name": "location 1 - level 2",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 2,
+          "items": [
+            { "name": "Dwaenor's fist hammer" },
+            { "name": "Golden fistula" },
+            { "name": "Uncanny Saw Cleaver" }
+          ]
+        }]
+      }
+    ]
   }, {
     "id": 2,
     "name": "My Third Dungeon",
     "glyph": "1sa8234lk",
-    "items": []
+    "levels": [
+      {
+        "name": 1,
+        "locations": [{
+          "name": "location 1 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "A Dwaenor's fist hammer" },
+            { "name": "B Golden fistula" },
+            { "name": "C Uncanny Saw Cleaver" }
+          ]
+        }, {
+          "name": "location 2 - level 1",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 1,
+          "items": [
+            { "name": "D Dwaenor's fist hammer" },
+            { "name": "E Golden fistula" },
+            { "name": "F Uncanny Saw Cleaver" }
+          ]
+        }]
+      }, {
+        "name": 2,
+        "locations": [{
+          "name": "location 1 - level 2",
+          "description": "lorem ipsum dolor sit amet.",
+          "level": 2,
+          "items": [
+            { "name": "Dwaenor's fist hammer" },
+            { "name": "Golden fistula" },
+            { "name": "Uncanny Saw Cleaver" }
+          ]
+        }]
+      }
+    ]
   }
 ]
